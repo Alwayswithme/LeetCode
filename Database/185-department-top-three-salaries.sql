b40c54ad01decc79ae1074710dc2f945067f6789
Alwayswithme <phx13ye@gmail.com>
2015-04-28 23:32:37 +0800

    finish sql

--- Database/185-department-top-three-salaries.sql
+++ Database/185-department-top-three-salaries.sql
@@ -1,6 +1,50 @@
 --
 -- Author     :  Ye Jinchang
--- Date       :  2015-04-24 16:03:04
+-- Date       :  2015-04-28 23:03:04
 -- Title      :  185 department top three salaries
 --
- 
+
+ /***********************************************************************
+
+The Employee table holds all employees. Every employee has an Id, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who earn the top three salaries in each of the department. For the above tables, your SQL query should return the following rows.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Randy    | 85000  |
+| IT         | Joe      | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+
+************************************************************************/
+
+
+-- Write your MySQL query statement below
+SELECT D.Name AS Department, E.Name AS Employee, E.Salary AS Salary
+FROM Employee E, Department D
+WHERE (SELECT COUNT(DISTINCT(Salary)) FROM Employee
+       WHERE DepartmentId = E.DepartmentId AND Salary > E.Salary) < 3
+AND E.DepartmentId = D.Id
+ORDER by E.DepartmentId, E.Salary DESC;
--- Database/196-delete-duplicate-emails.sql
+++ Database/196-delete-duplicate-emails.sql
@@ -3,4 +3,31 @@
 -- Date       :  2015-04-24 16:03:04
 -- Title      :  196 delete duplicate emails
 --
- 
+
+/*******************************************************************
+
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+
+*********************************************************************/
+
+
+-- Write your MySQL query statement below
+DELETE P1 FROM Person P1 INNER JOIN Person P2
+WHERE P1.Email = P2.Email AND P1.Id > P2.Id
