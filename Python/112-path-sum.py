7680740eaca90b0b6ed8692e6d4c1006dc72d820
Alwayswithme <phx13ye@gmail.com>
2015-09-24 23:01:46 +0800

    112

---
+++ Python/112-path-sum.py
@@ -0,0 +1,41 @@
+#!/bin/python
+#
+# Author     :  Ye Jinchang
+# Date       :  2015-09-24 23:00:29
+# Title      :  112 path sum
+
+#  Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+# For example:
+# Given the below binary tree and sum = 22,
+# 
+#               5
+#              / \
+#             4   8
+#            /   / \
+#           11  13  4
+#          /  \      \
+#         7    2      1
+# 
+# return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if root.val == sum and not root.left and not root.right:
+            return True
+        sum -= root.val
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
